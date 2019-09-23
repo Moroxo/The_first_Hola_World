28be1e7ab472e76df6feb648f1e60819846ef11d
Sebastian <seba.sanchezs@alumnos.duoc.cl>
2019-09-23 12:28:34 -0300

    Agregamos el hola mundo basico

---
+++ miprimerHOLA.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# la primera linea es para mi linux! no es necesario en windows
+# Este es un hola mundo en python
+# Guardar como archivo .py en la carpeta que tenemos con git
+
+print('Hola Mundo')
