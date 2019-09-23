952a9add5278a581a817871aa2453fb94ecd0978
Sebastian <seba.sanchezs@alumnos.duoc.cl>
2019-09-23 12:48:07 -0300

    Agregamos un ejemplo de uso de ciclos

---
+++ ejemplo_ciclo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Guardar como archivo .py en la carpeta que tenemos con git
+# Guardar como archivo"ejemplo_cilclo.py"
+# Los espacios si importan
+i = 6
+ while i > 0 :
+ 		print(i)
+ 			i = i - 1 
+
+ciudades = ["Arica", "Iquique", "Amtofagasta", "Copiapo"]
+for ciudad in ciudades:
+		print(ciudad)
